dcfb8249ad691a471b8938fe4e7d587a5f7f51ca
Gabriel Menezes <gabrielsm90@gmail.com>
2018-01-29 09:03:29 -0200

    Fixing database name on settings

--- news_crawler/scraping/crawler/settings.py
+++ news_crawler/scraping/crawler/settings.py
@@ -7,12 +7,11 @@ BOT_NAME = 'crawler'
 SPIDER_MODULES = ['crawler.spiders']
 NEWSPIDER_MODULE = 'crawler.spiders'
 
-# Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
 ITEM_PIPELINES = {
     'crawler.pipelines.MongoWriterPipeline': 1,
 }
 
-MONGO_URI = 'mongodb://gabriel_menezes:Flu#1902@ds119028.mlab.com:19028/crawler'
-MONGO_DATABASE = "crawler"
+MONGO_URI = 'mongodb://gabriel_menezes:Flu#1902@ds119028.mlab.com:19028/news_crawler'
+MONGO_DATABASE = "news_crawler"
